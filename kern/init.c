41a9d6877d3564a20d3de3f2e2153c6fcf220e65
stw <stw@ubuntu.(none)>
2014-02-27 08:53:41 -0800

    lab1

--- kern/init.c
+++ kern/init.c
@@ -12,6 +12,7 @@ void
 test_backtrace(int x)
 {
 	cprintf("entering test_backtrace %d\n", x);
+
 	if (x > 0)
 		test_backtrace(x-1);
 	else
@@ -34,6 +35,26 @@ i386_init(void)
 	cons_init();
 
 	cprintf("6828 decimal is %o octal!\n", 6828);
+
+	/*
+	 * int x = 1, y = 3, z = 4;
+	cprintf("x %d, y %x, z %d\n", x, y, z);
+
+	unsigned int i = 0x00646c72;
+	cprintf("H%x Wo%s\n",57616, &i);
+
+	cprintf("x=%d y=%d\n", 3);
+	*/
+	
+	cprintf("\x1b[1,32m123-%d-%c \x1b[0m\n",32,'o');	
+	cprintf("\x1b[1,31m123-%d-%c \x1b[0m\n",32,'o');
+	cprintf("\x1b[1,34m123-%d-%c \x1b[0m\n",32,'o');
+	cprintf("\x1b[32m123-%d-%c \x1b[0m\n",32,'o');	
+	cprintf("\x1b[31m123-%d-%c \x1b[0m\n",32,'o');
+	cprintf("\x1b[34m123-%d-%c \x1b[0m\n",32,'o');
+	cprintf("\x1b[42m123-%d-%c \x1b[0m\n",32,'o');	
+	cprintf("\x1b[41m123-%d-%c \x1b[0m\n",32,'o');
+	cprintf("\x1b[44m123-%d-%c \x1b[0m\n",32,'o');
 
 	// Test the stack backtrace function (lab 1 only)
 	test_backtrace(5);
